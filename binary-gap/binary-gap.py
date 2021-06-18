b31db47ca672551ae74100faca0c993dfe969665
DaeHwi Kim <58774316+kdh92417@users.noreply.github.com>
2021-06-18 12:14:59 +0900

    Time: 32 ms (61.44%), Space: 14.1 MB (73.99%) - LeetHub

--- binary-gap/binary-gap.py
+++ binary-gap/binary-gap.py
@@ -1,19 +1,11 @@
 class Solution:
     def binaryGap(self, n: int) -> int:
-        bin_list = collections.deque(list(map(int, bin(n)[2:])))
-        stack = []
-        count = 1
-        max_count = []
-        while len(bin_list) > 0:
-            cur = bin_list.popleft()
-            if cur == 1 and cur not in stack:
-                stack.append(cur)
-            elif cur == 0 and 1 in stack:
-                count += 1
-            elif cur == 1 and cur in stack:
-                max_count.append(count)
-                count = 1
-
-
-
-        return 0 if max_count == [] else max(max_count)
+        pre = dis = 0
+        
+        for i, v in enumerate(bin(n)[2:]):
+            if v == "1":
+                dis = max(dis, i-pre)
+                pre = i
+                
+        return dis    
+            

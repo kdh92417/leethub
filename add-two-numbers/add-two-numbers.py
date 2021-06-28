9766d04d92ed6eabef26a127c89653eee1c0bb58
DaeHwi Kim <58774316+kdh92417@users.noreply.github.com>
2021-06-28 11:08:16 +0900

    Time: 84 ms (16.98%), Space: 14.5 MB (11.14%) - LeetHub

--- add-two-numbers/add-two-numbers.py
+++ add-two-numbers/add-two-numbers.py
@@ -1,33 +1,47 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+    # class ListNode:
+    #     def __init__(self, val=0, next=None):
+    #         self.val = val
+    #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+    # 연결 리스트 뒤집기
+    def reverseList(self, head: ListNode) -> ListNode:
+        node, prev = head, None
         
-        result = ''
-        l1_st = ''
-        l2_st = ''
-    
-        while l1 or l2:
-            if l1:
-                l1_st = str(l1.val) + l1_st
-                l1 = l1.next
-            if l2:
-                l2_st = str(l2.val) + l2_st
-                l2 = l2.next
+        while node:
+            next, node.next = node.next, prev
+            prev, node = node, next
             
-        result = list(str(int(l1_st)+int(l2_st)))
-        cur = int(result.pop())
-        l = ListNode(val=cur)
-        temp = l
+        return prev
+            
+    # 연결 리스트를 파이썬 리스트로 변환
+    def toList(self, node: ListNode) -> ListNode:
+        list = []
+        while node:
+            list.append(node.val)
+            node = node.next
+        return list
+    
+    # 파이썬 리스트를 연결 리스트로 변환
+    def toReverseLinkedList(self, result):
+        node = ListNode(result.pop())
+        temp = node
+        
         while result:
-            cur = int(result.pop())
-            temp.next = ListNode(cur)
+            temp.next = ListNode(result.pop())
             temp = temp.next
             
-        return l
+        return node
+    
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        reverse_l1 = self.toList(self.reverseList(l1))
+        reverse_l2 = self.toList(self.reverseList(l2))
+        
+        result = int(''.join(str(s) for s in reverse_l1)) + \
+            int(''.join(str(s) for s in reverse_l2))
+        
+        return self.toReverseLinkedList(list(str(result)))
+        
             
         
         

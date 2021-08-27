7612b52612bbeed5829f361dcc5161b79b203719
DaeHwi Kim <58774316+kdh92417@users.noreply.github.com>
2021-08-27 10:23:46 +0900

    Time: 52 ms (5.18%), Space: 14.4 MB (5.34%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,24 +1,29 @@
-from itertools import product
-
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if digits == "":
-            return digits
+        if not digits:
+            return []
         
-        dig = {
-            2 : ['a', 'b', 'c'],
-            3 : ['d', 'e', 'f'],
-            4 : ['g', 'h', 'i'],
-            5 : ['j', 'k', 'l'],
-            6 : ['m', 'n', 'o'],
-            7 : ['p', 'q', 'r', 's'],
-            8 : ['t', 'u', 'v'],
-            9 : ['w', 'x', 'y', 'z']
+        key_board = {
+            2 : "abc",
+            3 : "def",
+            4 : "ghi",
+            5 : "jkl",
+            6 : "mno",
+            7 : "pqrs",
+            8 : "tuv",
+            9 : "wxyz"
         }
         
-        if len(digits) < 2:
-            return dig[int(digits)]
+        result = []
         
-        com = product(*[dig[int(i)] for i in digits])
-        result = [''.join(c) for c in com]
+        def dfs(index, path):
+            if len(path) == len(digits):
+                result.append(path)
+                return
+            
+            for i in range(index, len(digits)):
+                for s in key_board[int(digits[i])]:
+                    dfs(i+1, path+s)
+        
+        dfs(0, '')
         return result

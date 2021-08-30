68f52298177358330c9ba28426b5cf3099a36171
DaeHwi Kim <58774316+kdh92417@users.noreply.github.com>
2021-08-30 14:04:09 +0900

    Time: 46 ms (7.67%), Space: 14.4 MB (5.25%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,11 +1,18 @@
-class Solution(object):
-    def letterCombinations(self, digits):
-        """
-        :type digits: str
-        :rtype: List[str]
-        """
-        interpret_digit = {
-            '1': '',
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        def dfs(idx, path):
+            if len(path) == len(digits):
+                res.append(path)
+                return
+            
+            for i in range(idx, len(digits)):
+                for letter in key_pad[digits[i]]:
+                    dfs(i + 1, path + letter)
+                    
+        if not digits:
+            return []
+        
+        key_pad = {
             '2': 'abc',
             '3': 'def',
             '4': 'ghi',
@@ -13,13 +20,13 @@ class Solution(object):
             '6': 'mno',
             '7': 'pqrs',
             '8': 'tuv',
-            '9': 'wxyz',
-            '0': ' '}
-        all_combinations = [''] if digits else []
-        for digit in digits:
-            current_combinations = list()
-            for letter in interpret_digit[digit]:
-                for combination in all_combinations:
-                    current_combinations.append(combination + letter)
-            all_combinations = current_combinations
-        return all_combinations
+            '9': 'wxyz'
+        }
+         
+        res = []
+        dfs(0, "")
+        
+        return res
+            
+        
+        

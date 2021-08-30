356cc577518a59c649a8ecd22567a34ab3caaec8
DaeHwi Kim <58774316+kdh92417@users.noreply.github.com>
2021-08-30 11:38:20 +0900

    Time: 32 ms (60.65%), Space: 14.2 MB (63.11%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,29 +1,25 @@
-class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
-            return []
-        
-        key_board = {
-            2 : "abc",
-            3 : "def",
-            4 : "ghi",
-            5 : "jkl",
-            6 : "mno",
-            7 : "pqrs",
-            8 : "tuv",
-            9 : "wxyz"
-        }
-        
-        result = []
-        
-        def dfs(index, path):
-            if len(path) == len(digits):
-                result.append(path)
-                return
-            
-            for i in range(index, len(digits)):
-                for s in key_board[int(digits[i])]:
-                    dfs(i+1, path+s)
-        
-        dfs(0, '')
-        return result
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        interpret_digit = {
+            '1': '',
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz',
+            '0': ' '}
+        all_combinations = [''] if digits else []
+        for digit in digits:
+            current_combinations = list()
+            for letter in interpret_digit[digit]:
+                for combination in all_combinations:
+                    current_combinations.append(combination + letter)
+            all_combinations = current_combinations
+        return all_combinations

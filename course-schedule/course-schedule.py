a79a84d1cf4ef1fe590bd819d9cd56b002e0ff76
DaeHwi Kim <58774316+kdh92417@users.noreply.github.com>
2021-09-29 18:33:06 +0900

    Time: 96 ms (81.12%), Space: 17.9 MB (8.00%) - LeetHub

---
+++ course-schedule/course-schedule.py
@@ -0,0 +1,34 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = collections.defaultdict(list)
+        for x, y in prerequisites:
+            graph[x].append(y)
+        
+        traced  = set()
+        visited = set()
+        
+        def dfs(i):
+            if i in traced:
+                return False
+            if i in visited:
+                return True
+            
+            
+            traced.add(i)
+            for y in graph[i]:
+                if not dfs(y):
+                    return False
+                
+            traced.remove(i)
+            visited.add(i)
+            
+            return True
+        
+        for i in list(graph):
+            if not dfs(i):
+                return False
+        
+        return True
+                
+            
+        

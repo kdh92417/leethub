994dc35d7962968cb976a58408ffd9267702e2e2
DaeHwi Kim <58774316+kdh92417@users.noreply.github.com>
2022-01-12 09:41:45 +0900

    Time: 288 ms (5.07%), Space: 19.5 MB (11.98%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -2,5 +2,9 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         palindrome = [i.lower() for i in s if i.isalnum()]
         
-        return palindrome == palindrome[::-1]
+        while len(palindrome) > 1:
+            if palindrome.pop(0) != palindrome.pop():
+                return False
+            
+        return True
             

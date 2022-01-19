e9a4196702bc331c0897ec11c53ba8c7c605e15c
DaeHwi Kim <58774316+kdh92417@users.noreply.github.com>
2022-01-19 22:55:58 +0900

    Time: 90 ms (11.86%), Space: 14.4 MB (76.15%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
@@ -1,8 +1,11 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        c_nums = collections.Counter(nums1)
-        result = set()
-        for num in nums2:
-            if c_nums[num] > 0:
-                result.add(num)
+        result: Set = set()
+        nums1.sort()
+        
+        for n2 in nums2:
+            n_idx = bisect.bisect_left(nums1, n2)
+            
+            if len(nums1) > n_idx and nums1[n_idx] == n2:
+                result.add(n2)
         return result

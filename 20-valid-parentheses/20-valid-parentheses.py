8cb46b7aeaae4f2ea3ea2d95fa473be19397c139
DaeHwi Kim <58774316+kdh92417@users.noreply.github.com>
2022-02-07 09:22:01 +0900

    Time: 62 ms (10.67%), Space: 13.8 MB (99.73%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,25 +1,16 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        
-        if len(s) % 2 == 1:
-            return False
-        
         bracket = {
-            '(': ')',
-            '[': ']',
-            '{': '}',
+            ')': '(',
+            ']': '[',
+            '}': '{',
         }
-        
         stack = []
         
         for b in s:
-            
-            if b in bracket:
-                stack.append(bracket[b])
-            elif b not in stack:
+            if b not in bracket:
+                stack.append(b)
+            elif not stack or bracket[b] != stack.pop():
                 return False
-
-            if stack and stack[-1] == b:
-                stack.pop()
-                
-        return False if stack else True
+            
+        return len(stack) == 0

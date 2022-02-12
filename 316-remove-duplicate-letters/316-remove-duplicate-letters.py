797b4ba8103cac3342a19a66b72790e09f3220dd
DaeHwi Kim <58774316+kdh92417@users.noreply.github.com>
2022-02-12 18:39:56 +0900

    Time: 108 ms (5.04%), Space: 14.1 MB (69.16%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.py
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.py
@@ -1,12 +1,14 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        print('sorted s : ', sorted(set(s)))
+        print('========= sorted s : ', sorted(set(s)), ' || s :', s)
+        
         for char in sorted(set(s)):
             suffix = s[s.index(char):]
-            print(char, s.index(char), suffix)
+            print('char: ' , char, ' || suffix : ', suffix, ' || set(s) : ', set(s))
             
             if set(s) == set(suffix):
-                print('parameter : ', suffix.replace(char, ''), 'set s : ', set(s))
-                return char + self.removeDuplicateLetters(suffix.replace(char, ''))
+                print('recursice Char: ', char)
+                result = char + self.removeDuplicateLetters(suffix.replace(char, ''))
+                return result
         return ''
             

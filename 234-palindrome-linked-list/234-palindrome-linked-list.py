dbd83e94f3d8c78f22c7278bddaf9b6c3088d983
DaeHwi Kim <58774316+kdh92417@users.noreply.github.com>
2022-03-27 16:56:07 +0900

    Time: 736 ms (98.05%), Space: 31.1 MB (95.17%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -5,14 +5,19 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        lst = collections.deque()
+        rev = None
+        slow = fast = head
         
-        while head:
-            lst.append(head.val)
-            head = head.next
-            
-        while len(lst) > 1:
-            if lst.popleft() != lst.pop():
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
+        
+        if fast:
+            slow = slow.next
+        
+        while rev:
+            if rev.val != slow.val:
                 return False
-            
+            rev, slow = rev.next, slow.next
+        
         return True

df71cdc54f8c996ee4b3c0b3e9df3ab4891ee831
DaeHwi Kim <58774316+kdh92417@users.noreply.github.com>
2022-04-24 17:10:58 +0900

    Time: 67 ms (60.38%), Space: 18.7 MB (40.75%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.py
+++ 110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -6,17 +6,19 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        
-        def check(node):
+
+        def search(node):
             if not node:
                 return 0
-            
-            left = check(node.left)
-            right = check(node.right)
+
+            left = search(node.left)
+            right = search(node.right)
             
             if left == -1 or right == -1 or abs(left - right) > 1:
                 return -1
-            
+
             return max(left, right) + 1
         
-        return check(root) != -1
+        return search(root) != -1
+        
+            

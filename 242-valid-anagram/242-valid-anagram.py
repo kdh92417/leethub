f1722a9ae2b6e1096e52cbf06ed32404f0bd861b
DaeHwi Kim <58774316+kdh92417@users.noreply.github.com>
2022-05-30 08:43:00 +0900

    Time: 59 ms (68.19%), Space: 15 MB (21.92%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,10 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_dict = collections.Counter(s)
-        t_dict = collections.Counter(t)
-        
-        for k, v in s_dict.items():
-            if v != t_dict[k]:
-                return False
-        
-        return sum(s_dict.values()) == sum(t_dict.values())
+        return sorted(s) == sorted(t)

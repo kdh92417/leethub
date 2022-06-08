71c9a07d507b0d7336652378a405315f36c6d147
DaeHwi Kim <58774316+kdh92417@users.noreply.github.com>
2022-06-08 09:45:39 +0900

    Time: 1753 ms (89.22%), Space: 30.6 MB (35.29%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -1,57 +1,16 @@
 class Solution:
     def maxSlidingWindow(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-# Checking for base case
-        if not nums:
-            return []
-        if k == 0:
-            return nums
-# Defining Deque and result list
-        deq = deque()
-        result = []
-        
-# First traversing through K in the nums and only adding maximum value's index to the deque.
-# Note: We are olny storing the index and not the value.
-# Now, Comparing the new value in the nums with the last index value from deque,
-# and if new valus is less, we don't need it
-
-        for i in range(k):
-            while len(deq) != 0:
-                if nums[i] > nums[deq[-1]]:
-                    deq.pop()
-                else:
-                    break
-
-            deq.append(i)
-            
-# Here we will have deque with index of maximum element for the first subsequence of length k.
-	
-# Now we will traverse from k to the end of array and do 4 things
-# 1. Appending left most indexed value to the result
-# 2. Checking if left most is still in the range of k (so it only allows valid sub sequence)
-# 3. Checking if right most indexed element in deque is less than the new element found, if yes we will remove it
-# 4. Append i at the end of the deque  (Not: 3rd and 4th steps are similar to previous for loop)
- 
-        for i in range(k, len(nums)):
-            result.append(nums[deq[0]])
-            
-            if deq[0] < i - k + 1:
-                deq.popleft()
-            
-            while len(deq) != 0:
-                if nums[i] > nums[deq[-1]]:
-                    deq.pop()
-                else:
-                    break
-            
-            deq.append(i)
-        
-#Adding the maximum for last subsequence
-        result.append(nums[deq[0]])
-    
-        return result
-        
+        from collections import deque
+        q = deque() # stores *indices*
+        res = []
+        for i, cur in enumerate(nums):
+            while q and nums[q[-1]] <= cur:
+                q.pop()
+            q.append(i)
+            # remove first element if it's outside the window
+            if q[0] == i - k:
+                q.popleft()
+            # if window has k elements add to results (first k-1 windows have < k elements because we start from empty window and add 1 element each iteration)
+            if i >= k - 1:
+                res.append(nums[q[0]])
+        return res

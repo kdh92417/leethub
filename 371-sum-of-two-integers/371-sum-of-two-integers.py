901abaa29435c946685c5275ff6f2aedc6da0274
DaeHwi Kim <58774316+kdh92417@users.noreply.github.com>
2022-06-15 09:48:22 +0900

    Time: 51 ms (25.79%), Space: 13.9 MB (12.82%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.py
+++ 371-sum-of-two-integers/371-sum-of-two-integers.py
@@ -1,34 +1,14 @@
 class Solution:
-    def getSum(self, a: int, b: int) -> int:
-        result = []
-        
+    def getSum(self, a: int, b: int) -> int:        
         MASK = 0xFFFFFFFF
         INT_MAX = 0x7FFFFFFF
         
-        a_bin = bin(a & MASK)[2:].zfill(32)
-        b_bin = bin(b & MASK)[2:].zfill(32)
-        carry = 0
-        
-        for i in range(32):
-            A = int(a_bin[31 - i])
-            B = int(b_bin[31 - i])
+        while b != 0:
+            a, b = (a ^ b) & MASK, ((a & b) << 1) & MASK
             
-            Q1 = A & B
-            Q2 = A ^ B
-            Q3 = Q2 & carry
-            current_sum = Q2 ^ carry
-            carry = Q1 | Q3
-            
-            result.append(str(current_sum))
+        if a > INT_MAX:
+            a = ~(a ^ MASK)
         
-        if carry == 1:
-            result.append('1')
-            
-        result = int(''.join(result[::-1]), 2) & MASK
-        
-        if result > INT_MAX:
-            result = ~(result ^ MASK)
-        
-        return result
+        return a
             
             

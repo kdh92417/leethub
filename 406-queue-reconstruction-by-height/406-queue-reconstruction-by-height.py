fdc3c4119da92edcede0e8433bfcd532e431557e
DaeHwi Kim <58774316+kdh92417@users.noreply.github.com>
2022-06-26 15:34:51 +0900

    Time: 181 ms (36.84%), Space: 14.7 MB (14.74%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
@@ -1,18 +1,13 @@
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        heapq.heapify(people)
-        result = [None] * len(people)
-
-        while people:
-            p = heapq.heappop(people)
-            value, order = p
-
-            for i in range(len(result)):
-                if result[i] is None and order == 0:
-                    result[i] = p
-                    break
-
-                if result[i] is None or (result[i] and result[i][0] >= value):
-                    order -= 1
-        return result
+        heap = []
+        result = []
         
+        for p in people:
+            heapq.heappush(heap, [-p[0], p[1]])
+            
+        while heap:
+            p = heapq.heappop(heap)
+            result.insert(p[1], [-p[0], p[1]])
+
+        return result
